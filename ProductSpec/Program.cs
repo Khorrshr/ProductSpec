9e65cc2d5a143125e9e87f6cbcd8ec6eaef20a8a
Khorrshr <finallytheemailwhichisnottaken@gmail.com>
2025-02-01 09:44:04 +0200

    v 1.0

--- ProductSpec/Program.cs
+++ ProductSpec/Program.cs
@@ -4,15 +4,27 @@ using System.Collections.Generic;
 class Program
 {
     static void Main()
-    {//
+    {
         // Create a list of strings
         List<string> based = new List<string> { "XY", "CDF", "CDF", "XYZ" };
         List<string> plus = new List<string> { "ABC", "CDF", "CDF", "CDF", "BY" };
+        List<string> extra = new List<string> { };
 
-        // Iterate over the list using foreach
+        Console.WriteLine("Base list:");
+        foreach (string item in based) { Console.Write(item + ", "); }
+
+        Console.WriteLine("\nPlus list:");
+        foreach (string item in plus) { Console.Write(item + ", "); }
+
+        Console.WriteLine("\n");
+        
         foreach (string plusCode in plus)
         {
-            Console.WriteLine(plusCode);
+            if (!based.Remove(plusCode))
+                extra.Add(plusCode);
         }
+        Console.WriteLine("List of PLUS codes not in BASE:");
+        foreach (var extraCode in extra) { Console.Write(extraCode + " "); }
+        
     }
 }
