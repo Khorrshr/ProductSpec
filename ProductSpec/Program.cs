e1aaeff8c6d61f39115ffe68055572de70226018
Khorrshr <finallytheemailwhichisnottaken@gmail.com>
2025-02-01 09:17:07 +0200

    Add project files.

---
+++ ProductSpec/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Create a list of strings
+        List<string> based = new List<string> { "XY", "CDF", "CDF", "XYZ" };
+        List<string> plus = new List<string> { "ABC", "CDF", "CDF", "CDF", "BY" };
+
+        // Iterate over the list using foreach
+        foreach (string plusCode in plus)
+        {
+            Console.WriteLine(plusCode);
+        }
+    }
+}
