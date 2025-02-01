d30241732098d9d938bec4d22dcb083071c870e3
Khorrshr <finallytheemailwhichisnottaken@gmail.com>
2025-02-01 09:17:58 +0200

    lists created

--- ProductSpec/Program.cs
+++ ProductSpec/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 class Program
 {
     static void Main()
-    {
+    {//
         // Create a list of strings
         List<string> based = new List<string> { "XY", "CDF", "CDF", "XYZ" };
         List<string> plus = new List<string> { "ABC", "CDF", "CDF", "CDF", "BY" };
