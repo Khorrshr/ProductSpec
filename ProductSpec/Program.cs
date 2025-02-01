38573019d67ee000d6b04ac799d6ddef5f0abc33
Khorrshr <finallytheemailwhichisnottaken@gmail.com>
2025-02-01 10:07:22 +0200

    object for product created

--- ProductSpec/Program.cs
+++ ProductSpec/Program.cs
@@ -1,11 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 
+public class Product
+{
+    
+    public List<string> Codes { get; set; }     
+    public string Name { get; set; }
+    public float Cost { get; set; }    
+    public string Description { get; set; }    
+    public Product()
+    {
+        Codes = new List<string>();
+    }    
+    public Product(List<string> codes, string name, float cost, string description)
+    {
+        Codes = codes;
+        Name = name;
+        Cost = cost;
+        Description = description;
+    }
+}
+
 class Program
 {
     static void Main()
-    {        
-        List<string> based = new List<string> { "ABC", "XY", "CDF", "CDF", "XYZ" };
+    {
+        Product product = new Product();
+
+        product.Codes = new List<string> { "ABC", "XY", "CDF", "CDF", "XYZ" };
+        product.Name = "Exceptional Double CDFer";
+        product.Cost = 249.95f;
+        product.Description = "Hotplug your XYZ whenever you want!";
+
+
+        //List<string> based = new List<string> { "ABC", "XY", "CDF", "CDF", "XYZ" };
+        List<string> based = product.Codes.ToList();
         List<string> plus = new List<string> { "ABC", "CDF", "CDF", "CDF", "BY" };
         List<string> extra = new List<string> { };
 
